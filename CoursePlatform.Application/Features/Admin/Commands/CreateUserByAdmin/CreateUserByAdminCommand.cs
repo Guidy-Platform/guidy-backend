f898c7f223a6516c3d03e039f7a39c0b3c380c85
Aya Samir <ayasamir1810@gmail.com>
2026-04-28 17:16:50 +0300

    admin can add users

---
+++ CoursePlatform.Application/Features/Admin/Commands/CreateUserByAdmin/CreateUserByAdminCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursePlatform.Application.Features.Admin.Commands.CreateUserByAdmin
+{
+    public record CreateUserByAdminCommand(
+      string Email,
+      string FirstName,
+      string LastName,
+      string Role,
+      string Password
+  ) : IRequest<Guid>;
+}
